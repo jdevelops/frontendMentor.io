56b918bebc1289d501a0348f19146459b3fdbd0a
Jędrzej <jdevelop9@gmail.com>
2022-11-27 16:04:30 +0100

    v0.1.0

---
+++ script.js
@@ -0,0 +1,14 @@
+let test = document.getElementById("cardBlur");
+let btn = document.getElementById("cardBtn");
+
+test.addEventListener("mouseover", function (event) {
+  event.target.style.filter = "blur(2px)";
+  event.target.style.filter.webkitFilter = "blur(2px)";
+  btn.style.display = "block";
+});
+
+test.addEventListener("mouseout", function (event) {
+  event.target.style.filter = "blur(0px)";
+  event.target.style.filter.webkitFilter = "blur(0px)";
+  btn.style.display = "none";
+});
